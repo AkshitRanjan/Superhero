bf6bf92e50c28044c740720e3b8f4c33c54078ac
AkshitRanjan <73226453+AkshitRanjan@users.noreply.github.com>
2020-12-29 17:38:45 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -40,3 +40,66 @@ function new_image(get_image) {
         canvas.add(block_Image_Object);   
     })
 }
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e) {
+    keyPressed = e.keyCode;
+    console.log(keyPressed);
+    if (e.shiftKey == true && keyPressed == '80') {
+        console.log("P and Shift pressed together");
+        block_Image_Width = block_Image_Width + 10;
+        block_Image_Height = block_Image_Height + 10;
+        document.getElementById("current_width").innerHTML = block_Image_Width;
+        document.getElementById("current_height").innerHTML = block_Image_Height;
+    }
+    
+    if (e.shiftKey == true && keyPressed == '77') {
+        console.log("M and Shift pressed together");
+        block_Image_Width = block_Image_Width - 10;
+        block_Image_Height = block_Image_Height - 10;
+        document.getElementById("current_width").innerHTML = block_Image_Width;
+        document.getElementById("current_height").innerHTML = block_Image_Height;
+    }
+
+    if (keyPressed == '70') {
+        new_image('ironman_face.png');
+        console.log("f");
+    }
+    
+    if (keyPressed == '66') {
+        new_image('spiderman_body.png');
+        console.log("b");
+    }
+    
+    if (keyPressed == '76') {
+        new_image('hulk_legs.png');
+        console.log("l");
+    }
+    
+    if (keyPressed == '82') {
+        new_image('thor_right_hand.png');
+        console.log("r");
+    }
+    
+    if (keyPressed == '70') {
+        new_image('captain_america_left_hand.png');
+        console.log("h");
+    }
+    
+    if (keyPressed == '38') {
+        up()
+        console.log("up");
+    }
+    if (keyPressed == '37') {
+        left()
+        console.log("left");
+    }
+    if (keyPressed == '39') {
+        right()
+        console.log("right");
+    }
+    if (keyPressed == '40') {
+        down()
+        console.log("down");
+    }
+}
